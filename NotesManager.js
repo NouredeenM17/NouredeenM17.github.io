313c27785c6fd70af2260c7b0ed5399dc290816d
Nouredeen Mahmoud <nourahm.17@gmail.com>
2023-12-27 19:15:33 +0300

    basic-new-node-popup-completed

--- NotesManager.js
+++ NotesManager.js
@@ -1,14 +1,17 @@
+// This class is responsible for creating notes and managing them all
 class NotesManager {
     constructor() {
       this.notes = [];
     }
   
+    // Creates a new node, adds it to the note array and returns it
     createNote(content) {
       const note = new Note(content);
       this.notes.push(note);
       return note;
     }
-  
+
+    // Searches for a note with the specified note ID 
     getNoteById(noteId) {
       return this.notes.find(note => note.id === noteId);
     }
--- main.js
+++ main.js
@@ -1,74 +1,83 @@
 // This event listener ensures that the code inside it runs
 // after the HTML content has been fully loaded and parsed.
 document.addEventListener('DOMContentLoaded', init);
+var notes_manager;
 
   function init(){
+        // Select all elements with the class 'column' and store them in the 'columns' variable
         const columns = document.querySelectorAll('.column');
-        // Select all elements with the class 'column' and store them in the 'columns' variable.
-      
+        
+        // Iterate over each column.
         columns.forEach(column => {
-          // Iterate over each column.
-      
+          
           column.addEventListener('dragover', function (e) {
+            // Prevent the default behavior of the browser when a dragged element is over the column
             e.preventDefault();
-            // Prevent the default behavior of the browser when a dragged element is over the column.
           });
       
           column.addEventListener('drop', function (e) {
+            // Prevent the default behavior of the browser when a dragged element is dropped on the column
             e.preventDefault();
-            // Prevent the default behavior of the browser when a dragged element is dropped on the column.
-      
+            
+            // Retrieve the note ID that was set during the dragstart event
             const noteId = e.dataTransfer.getData('text/plain');
-            // Retrieve the data (note ID) that was set during the dragstart event.
-      
+            
+            // Get the note object using the note ID
             const note = notes_manager.getNoteById(noteId);
-            // Get the note element using the retrieved ID.
       
+            // Append the note element to the column where it was dropped
             column.appendChild(note.element);
-            // Append the note to the column where it was dropped.
-
-            const addButton = column.querySelector('.add-button');
-            addButton.addEventListener('click', function () {
-            openNewNotePopup(column.id);
-            });
           });
         });
       
         // Initializing notes manager object
-        const notes_manager = new NotesManager();
+        notes_manager = new NotesManager();
 
         // Add a default note
-        var test_note = notes_manager.createNote("This is a note!");
-        addNoteToColumn(test_note,'column1');
+        var default_note = notes_manager.createNote("This is a note!");
+        addNoteToColumn(default_note,'column1');
       }
 
+      // Function to add a note to a specified column
       function addNoteToColumn(note ,columnId) {
-        // Function to add a new note to a specified column.
-    
+        
+        // Get the column element using the specified ID
         const column = document.getElementById(columnId);
-        // Get the column element using the specified ID.
-    
+        
+      // Append the note element to the specified column
         column.appendChild(note.element);
-        // Append the note to the specified column.
       }
 
+      // Function to open the popup for a specific column.
       function openNewNotePopup(columnId) {
-        // Function to open the popup for a specific column.
-    
+        
+        // Create popup element
         const popup = document.createElement('div');
         popup.classList.add('popup');
-    
+        
+        // Create the close button
         const closeButton = document.createElement('button');
-        closeButton.innerText = 'Close';
+        closeButton.innerText = 'X';
         closeButton.addEventListener('click', function () {
           document.body.removeChild(popup);
         });
     
-        const popupContent = document.createElement('div');
-        popupContent.innerText = `Add a note to ${columnId}`;
+        // Create text area element
+        const textarea = document.createElement('textarea');
+
+        // Create save button element
+        const saveButton = document.createElement('button');
+        saveButton.innerText = 'Save';
+        saveButton.addEventListener('click', () => {
+          content = textarea.value;
+          var newNote = notes_manager.createNote(content);
+          document.body.removeChild(popup);
+          addNoteToColumn(newNote, columnId);
+        });
     
         popup.appendChild(closeButton);
-        popup.appendChild(popupContent);
+        popup.appendChild(textarea);
+        popup.appendChild(saveButton);
     
         document.body.appendChild(popup);
       }
