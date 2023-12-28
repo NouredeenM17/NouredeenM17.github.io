a1b44a963002792a988fa6f36d91548ad5259ad0
Nouredeen Mahmoud <nourahm.17@gmail.com>
2023-12-28 23:45:38 +0300

    edit-note-implementation-completed

--- Note.js
+++ Note.js
@@ -13,13 +13,21 @@ class Note {
       note.setAttribute('draggable', 'true');
       note.setAttribute('id', this.id);
       note.addEventListener('dragstart', this.handleDragStart.bind(this));
-  
+
+      // Create the edit button
+      const editButton = document.createElement('button');
+      editButton.innerText = 'Edit';
+      editButton.addEventListener('click', this.openEditNotePopup.bind(this));
+
       const contentDiv = document.createElement('div');
+      contentDiv.classList.add('content-div');
       contentDiv.innerText = this.content;
   
       const dateDiv = document.createElement('div');
+      dateDiv.classList.add('date-created');
       dateDiv.innerText = `Created on: ${this.formatDate(this.dateCreated)}`;
-  
+
+      note.appendChild(editButton);
       note.appendChild(contentDiv);
       note.appendChild(dateDiv);
   
@@ -30,9 +38,55 @@ class Note {
       e.dataTransfer.setData('text/plain', this.id);
     }
   
+    // Formats the date into string
     formatDate(date) {
-      // Add your date formatting logic here
       return date.toLocaleString();
     }
+
+    // Function to open the popup for a specific column.
+    openEditNotePopup() {
+        
+      // Create popup element
+      const popup = document.createElement('div');
+      popup.classList.add('popup');
+      
+      // Create the close button
+      const closeButton = document.createElement('button');
+      closeButton.innerText = 'X';
+      closeButton.addEventListener('click', function () {
+        document.body.removeChild(popup);
+      });
+  
+      // Create text area element and place current content
+      const textarea = document.createElement('textarea');
+      textarea.classList.add('text-editor');
+      textarea.value = this.content;
+
+      // Create save button element
+      const saveButton = document.createElement('button');
+      saveButton.innerText = 'Save';
+      
+      saveButton.addEventListener('click', () => {
+        this.content = textarea.value;
+        document.body.removeChild(popup);
+        this.updateNoteElementContent();
+      });
+  
+      popup.appendChild(closeButton);
+      popup.appendChild(textarea);
+      popup.appendChild(saveButton);
+  
+      document.body.appendChild(popup);
+    }
+
+    updateNoteElementContent() {
+      // Update the content in the note element
+      const contentDiv = this.element.querySelector('.content-div');
+      
+      if (contentDiv) {
+        contentDiv.innerText = this.content;
+      }
+    }
+
   }
   
